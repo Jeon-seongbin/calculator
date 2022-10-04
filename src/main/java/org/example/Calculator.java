3c2236ec5ee952fda40c10423e597983ce35a83f
wk0012345 <wk0012345@naver.com>
2022-10-04 22:54:42 +0900

    add readme.md

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -18,14 +18,20 @@ public class Calculator {
 //        return 0;
 //    }
 
-
-    private static final List<NewArithmeticOperator> arithmeticOperatorList = List.of(new AdditionOperator(), new SubtractionOperator(), new MultiplyOperator(), new DivitionOperator());
+    private static final List<NewArithmeticOperator> arithmeticOperatorList =
+            List.of(
+                    new AdditionOperator(),
+                    new SubtractionOperator(),
+                    new MultiplyOperator(),
+                    new DivitionOperator()
+            );
 
     public static int calculator(int operand1, String operator, int operand2) {
+
         return arithmeticOperatorList.stream()
-                .filter(arithmeticOperatorList -> arithmeticOperatorList.surpports(operator))
-                .map(ArithmeticOperator -> ArithmeticOperator.calculator(operand1, operand2))
+                .filter(arithmeticOperator -> arithmeticOperator.surpports(operator))
+                .map(arithmeticOperator -> arithmeticOperator.calculator(operand1, operand2))
                 .findFirst()
-                .orElseThrow(() -> new IllegalArgumentException("올바른 사칙연산이 아닙니다"));
+                .orElseThrow(() -> new IllegalArgumentException("올바른 사칙 연산이 아닙니다"));
     }
 }
